1006c737b7acfa77d45fe1ac0c6024d1a118e8bc
kazuki.shida <kazuki.shida1226@gmail.com>
2022-06-01 11:01:28 +0000

    carrierwave

--- Gemfile
+++ Gemfile
@@ -44,7 +44,9 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'carrierwave', '~> 2.0'
+gem 'carrierwave', '~> 1.3', '>= 1.3.1'
+
+gem 'cloudinary'
 
 gem 'pagy'
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,9 +4,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fill: [700, 600, "Center"]
   # Choose what kind of storage to use for this uploader:
-  storage :file
   # storage :fog
   
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
   
 
   # Override the directory where uploaded files will be stored.
