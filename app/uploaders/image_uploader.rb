e29b88c89ed36259dc56fef032b053e3bf9a313c
kazuki.shida <kazuki.shida1226@gmail.com>
2022-06-01 12:49:43 +0000

    photo

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fill: [700, 600, "Center"]
+  process resize_to_fill: [800, 600, "Center"]
   # Choose what kind of storage to use for this uploader:
   # storage :fog
   
@@ -51,7 +51,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  
-  
-  
 end
